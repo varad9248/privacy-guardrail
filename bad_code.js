12165a2ea27a31eae683591de251591928cf4bce
varad9248 <149306292+varad9248@users.noreply.github.com>
2026-06-11 14:31:37 +0530

    Fix security violation in user authentication
    Removed logging of plain-text PII and passwords for security.

--- bad_code.js
+++ bad_code.js
@@ -3,11 +3,8 @@ function authenticateUser(req, res) {
     while(1){
     const userEmail = req.body.email;
     const userPassword = req.body.password;
-
     // SECURITY VIOLATION: Logging plain-text PII and passwords
     console.log("Attempting login for user: " + userEmail + " with password: " + userPassword);
-    
-    
     db.verify(userEmail, userPassword);
     }
 }
